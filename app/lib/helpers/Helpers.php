48655434387c88f3698579ff0ef791434b751389
Aleksandr Golubev <gulkinnos@gmail.com>
2020-02-22 10:32:46 -0500

    Wrap helpers functions to class to avoid using global functions.

---
+++ app/lib/helpers/Helpers.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Aleksandr Golubev <aka>gulkinnos@gmail.com
+ * Date: 5/20/2018
+ * Time: 7:28 PM
+ */
+
+class Helpers
+{
+
+	public static function dnd($data) {
+		echo '<pre>';
+		var_dump($data);
+		echo '</pre>';
+		die;
+	}
+
+	public static function sanitize() {
+
+	}
+}
