7bf0a12fbab7e38ed84587b8362e74db02ed9f5a
Aleksandr Golubev <gulkinnos@gmail.com>
2020-02-23 14:28:47 -0500

    Login from cookie and reformatting code

--- index.php
+++ index.php
@@ -8,21 +8,28 @@ require_once(ROOT . DS . 'config' . DS . 'config.php');
 require_once(ROOT . DS . 'app' . DS . 'lib' . DS . 'helpers' . DS . 'functions.php');
 
 //Autoload classes
-function autoload($className)
-{
-    if (file_exists(ROOT . DS . 'core' . DS . $className . '.php')) {
-        require_once(ROOT . DS . 'core' . DS . $className . '.php');
-    } elseif (file_exists(ROOT . DS . 'app' . DS . 'controllers' . DS . $className . '.php')) {
-        require_once(ROOT . DS . 'app' . DS . 'controllers' . DS . $className . '.php');
-    } elseif (file_exists(ROOT . DS . 'app' . DS . 'models' . DS . $className . '.php')) {
-        require_once(ROOT . DS . 'app' . DS . 'models' . DS . $className . '.php');
-    }
+function autoload($className) {
+	if(file_exists(ROOT . DS . 'core' . DS . $className . '.php')) {
+		require_once(ROOT . DS . 'core' . DS . $className . '.php');
+	}
+	elseif(file_exists(ROOT . DS . 'app' . DS . 'controllers' . DS . $className . '.php')) {
+		require_once(ROOT . DS . 'app' . DS . 'controllers' . DS . $className . '.php');
+	}
+	elseif(file_exists(ROOT . DS . 'app' . DS . 'models' . DS . $className . '.php')) {
+		require_once(ROOT . DS . 'app' . DS . 'models' . DS . $className . '.php');
+	}
 }
 
 spl_autoload_register('autoload');
 session_start();
 
+//Helpers::turnOnErrorReporting();
+
 $url = isset($_SERVER['PATH_INFO']) ? explode('/', ltrim($_SERVER['PATH_INFO'], '/')) : [];
+
+if(!Session::exists(CURRENT_USER_SESSION_NAME) && Cookie::exists(REMEMBER_ME_COOKIE_NAME)) {
+	Users::loginUserFromCookie();
+}
 
 
 //Route the request
