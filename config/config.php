7a3b306c08f3fc4d53a2e71a3e5fd33fe17d998e
Aleksandr Golubev <gulkinnos@gmail.com>
2020-02-23 14:37:50 -0500

    Changed config structure

---
+++ config/config.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Aleksandr Golubev <aka>gulkinnos@gmail.com
+ * Date: 5/20/2018
+ * Time: 7:45 PM
+ */
+
+define('DEBUG', true);
+
+define('DB_NAME',     'DB_NAME'); //database name
+define('DB_USER',     'DB_USER'); // database username
+define('DB_PASSWORD', 'DB_PASSWORD'); //database password
+define('DB_HOST',      '127.0.0.1'); // database host ***use IP address to avoid DNS lookup
+
+
+define('DEFAULT_CONTROLLER', 'Home'); //default controller if there isn`t one defined in the url
+define('DEFAULT_LAYOUT', 'default'); //If no layout is set in the controller use this layout
+
+define('PROOT', '/'); //set this to '/' for a life server.
+
+define('SITE_TITLE', 'Gulkinnos MVC Framework'); // This will be used if no site title is set
+
+define('CURRENT_USER_SESSION_NAME', 'fsdfdsfdsfdfsd435435fdsffrt4545'); //session name for logged in users
+define('REMEMBER_ME_COOKIE_NAME', 'dsfdfdfdf454543dgfgfd43'); //cookie name for logged in user remember me
+define('REMEMBER_ME_COOKIE_EXPIRY', 2592000); //time in seconds for remember me cookie to live (30 days)
